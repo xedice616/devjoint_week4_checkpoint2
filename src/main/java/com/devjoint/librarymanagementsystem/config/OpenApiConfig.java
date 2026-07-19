8c1e9ca4fe3849c2725b71cd7c6b8751ff0a7d07
xedice <xedicee616@gmail.com>
2026-07-19 19:49:26 +0400

    Add Swagger/OpenAPI documentation for REST API

--- src/main/java/com/devjoint/librarymanagementsystem/config/OpenApiConfig.java
+++ src/main/java/com/devjoint/librarymanagementsystem/config/OpenApiConfig.java
@@ -1,4 +1,39 @@
 package com.devjoint.librarymanagementsystem.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI libraryManagementOpenAPI() {
+
+        return new OpenAPI()
+
+                .info(new Info()
+
+                        .title("Library Management System API")
+
+                        .description("REST API for managing Authors, Books, Members and Loans.")
+
+                        .version("1.0.0")
+
+                        .contact(new Contact()
+                                .name("Khadija Ahmadova")
+                                .email("xedicee616@email.com"))
+
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Repository")
+                        .url("https://github.com/xedice616/devjoint_week1_checkpoint6"));
+    }
 }
